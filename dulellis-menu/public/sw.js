df804af9372425c27b820055322740d11b3abcf7
Dulelis <edijpasa@yahoo.com.br>
2026-07-22 00:15:27 -0300

    Remove fundo preto do ícone PWA

--- dulellis-menu/public/sw.js
+++ dulellis-menu/public/sw.js
@@ -1,6 +1,6 @@
-const STATIC_CACHE = "dulellis-static-v6";
-const RUNTIME_CACHE = "dulellis-runtime-v6";
-const IMAGE_CACHE = "dulellis-images-v6";
+const STATIC_CACHE = "dulellis-static-v7";
+const RUNTIME_CACHE = "dulellis-runtime-v7";
+const IMAGE_CACHE = "dulellis-images-v7";
 const OFFLINE_URL = "/offline";
 const APP_SHELL = [
   "/",
@@ -8,10 +8,10 @@ const APP_SHELL = [
   "/manifest.webmanifest",
   "/favicon.ico",
   "/logo.png",
-  "/icon-192-v2.png",
-  "/icon-512-v2.png",
-  "/icon-512-maskable-v2.png",
-  "/apple-touch-icon-v2.png",
+  "/icon-192-v3.png",
+  "/icon-512-v3.png",
+  "/icon-512-maskable-v3.png",
+  "/apple-touch-icon-v3.png",
 ];
 
 self.addEventListener("install", (event) => {
